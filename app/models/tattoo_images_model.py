e7559df6f73e45f97db4839c1f7eef85c7b2bb3c
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-03 14:49:43 -0300

    feat: add two column (image_mimetype, image_name) and renamed image for image_bin

--- app/models/tattoo_images_model.py
+++ app/models/tattoo_images_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, LargeBinary
+from sqlalchemy import Column, ForeignKey, LargeBinary, String
 from app.configs.database import db
 from dataclasses import dataclass
 from uuid import uuid4
@@ -10,9 +10,15 @@ class TattooImage(db.Model):
 
     id: str
     id_tattoo: str
+    image_name: str
+    image_bin: str
+    iamge_mimetype: str
 
     __tablename__ = "tattoo_images"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    image = Column(LargeBinary)
+    image_bin = Column(LargeBinary, nullable=False)
+    image_name = Column(String, nullable=False)
+    iamge_mimetype = Column(String, nullable=False)
+
     id_tattoo = Column(UUID(as_uuid=True), ForeignKey("tattoos.id"))
