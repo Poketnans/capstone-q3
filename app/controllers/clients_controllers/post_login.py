7fddd955ff43c2028cd85d444f16ad056fbabc37
Pedro <pedromenimen@gmail.com>
2022-03-03 11:54:06 -0300

    fix: some lines changed

--- app/controllers/clients_controllers/post_login.py
+++ app/controllers/clients_controllers/post_login.py
@@ -2,31 +2,29 @@ from http import HTTPStatus
 from flask import request
 from flask_jwt_extended import create_access_token
 from app.models.clients_model import Client
-from datetime import timedelta
-from app.services import fixed_values_eval, payload_eval
+from app.services import payload_eval
 import werkzeug.exceptions
+from app.configs import Config
 
-
-def post_login() -> dict:
+def post_login():
     data = request.get_json()
     try:
-        fixed_values_eval.fixed_values_eval(payload_eval.payload_eval(
+        clean_data = payload_eval.payload_eval(
             data=data,
             email=str,
             password=str,
             optional=[]
-        ))
-
-        user = Client.query.filter_by(email=data["email"]).first_or_404(
+        )
+        
+        user = Client.query.filter_by(email=clean_data["email"]).first_or_404(
             description={"msg": "user not found"})
 
-        if not user.check_password(data["password"]):
+        if not user.check_password(clean_data["password"]):
             return {"msg": "wrong password"}, HTTPStatus.FORBIDDEN
 
         token = create_access_token(
-            identity=user, expires_delta=timedelta(hours=1))
+            identity=user, expires_delta=Config.JWT_ACCESS_TOKEN_EXPIRES)
 
         return {"access token": token}
     except werkzeug.exceptions.NotFound as e:
         return e.description, HTTPStatus.NOT_FOUND
-    
