b2376b91801f4400e9dfe3d6beb5c0135b71e1bb
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-03 14:45:55 -0300

    refactor model, add relationship Tatoo and TatooImages. Remove column duration

--- app/models/tattoos_model.py
+++ app/models/tattoos_model.py
@@ -1,10 +1,11 @@
 from sqlalchemy import (
-    Column, ForeignKey, String, Integer, Text, Boolean
+    Column, ForeignKey, String, Text, Boolean
 )
 from app.configs.database import db
 from dataclasses import dataclass
 from uuid import uuid4
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 
 @dataclass
@@ -12,20 +13,20 @@ class Tattoo(db.Model):
 
     id: str
     size: str
-    duration: int
     colors: bool
     body_parts: str
     id_client: str
     id_tattooist: str
-    id_event: str
+    id_session: str
 
     __tablename__ = "tattoos"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     size = Column(String, nullable=False)
-    duration = Column(Integer, nullable=False)
-    colors = Column(Boolean)
+    colors = Column(Boolean, nullable=False)
     body_parts = Column(Text)
     id_client = Column(UUID(as_uuid=True), ForeignKey("clients.id"))
     id_tattooist = Column(UUID(as_uuid=True), ForeignKey("tattooists.id"))
-    id_event = Column(UUID(as_uuid=True), ForeignKey("events.id"))
+    id_session = Column(UUID(as_uuid=True), ForeignKey("sessions.id"))
+
+    image_models = relationship("TattooImage", uselist=True)
