a5ea1374e254f159eb689e4bbc953a33f39d75b9
Ricardo Lima <ricardo.engp@gmail.com>
2022-03-04 11:05:51 -0300

    fix: commiting minor fixing in clients model relationship

--- app/models/clients_model.py
+++ app/models/clients_model.py
@@ -41,10 +41,7 @@ class Client(db.Model):
     image_name = Column(String)
     image_bin = Column(LargeBinary)
     image_mimetype = Column(String)
-
-    tattoos = relationship("Tattoo", backref=backref(
-        "client", uselist=False), uselist=True)
-
+    
     @property
     def url_image(self):
         return self.url_image
