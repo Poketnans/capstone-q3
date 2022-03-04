a955e7f9be298b2324e5db10a53e897e1fa0ff53
Ricardo Lima <ricardo.engp@gmail.com>
2022-03-04 18:03:57 -0300

    feature: get_all clients implemented

--- app/routes/clients_blueprint.py
+++ app/routes/clients_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from app.controllers.clients_controllers import post_login, post_create
+from app.controllers.clients_controllers import post_login, post_create, get_all
 
 bp_clients = Blueprint("clients", __name__, url_prefix="/clients")
 bp_clients.post("/login")(post_login)
 bp_clients.post("")(post_create)
+bp_clients.get("")(get_all)
