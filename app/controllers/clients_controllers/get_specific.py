315db1e870cf2117ba4679fefc5da2a2411e4f0e
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-04 17:38:49 -0300

    feat: get specific

--- app/controllers/clients_controllers/get_specific.py
+++ app/controllers/clients_controllers/get_specific.py
@@ -1,18 +1,10 @@
 from flask import jsonify
-from app.models import Client
 from http import HTTPStatus
 from flask_jwt_extended import jwt_required, get_jwt_identity
-
-from sqlalchemy.orm.exc import NoResultFound
 
 
 @jwt_required()
 def get_specific():
-    try:
-        client = get_jwt_identity()
-        #id = "b3abc756-ef9f-48cf-a538-7a0c34fa3bff"
-        #client = Client.query.filter_by(id=id).first()
+    client = get_jwt_identity()
 
-        return jsonify(client), HTTPStatus.OK
-    except Exception as e:
-        raise e
+    return jsonify(client), HTTPStatus.OK
