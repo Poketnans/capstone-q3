17fd9ba294e78717db3d57d3990358c1d603a1b2
Etnan Sousa <poketnans@gmail.com>
2022-03-04 19:28:02 -0300

    fix: ajusts jwt required

--- app/controllers/clients_controllers/get_all.py
+++ app/controllers/clients_controllers/get_all.py
@@ -1,15 +1,17 @@
 from http import HTTPStatus
 
 from flask import jsonify
+from flask_jwt_extended import jwt_required
 
 from app.models.clients_model import Client
 
 
+@jwt_required()
 def get_all():
-    
+
     clients = Client.query.all()
-    
+
     if not clients:
         return jsonify([]), HTTPStatus.OK
-    
+
     return jsonify(clients), HTTPStatus.OK
