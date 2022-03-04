06ad687b347400eb6d7230b2dfa72e15fd5bce2d
Etnan Sousa <poketnans@gmail.com>
2022-03-04 18:31:18 -0300

    fix: ajusts some import errors

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 
-from .orders_blueprint import bp_orders
 from .storage_blueprint import bp_storage
 from .tattooists_blueprint import bp_tattooists
 from .tattoos_blueprint import bp_tattoos
+from .clients_blueprint import bp_clients
 
 
 def init_app(app: Flask) -> None:
     ''' Registra as blueprints '''
-    
-    app.register_blueprint(bp_orders)
-    app.register_blueprint(bp_storage)    
+
+    app.register_blueprint(bp_storage)
     app.register_blueprint(bp_tattooists)
     app.register_blueprint(bp_tattoos)
+    app.register_blueprint(bp_clients)
