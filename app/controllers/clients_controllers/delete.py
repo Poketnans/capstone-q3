c132fe49e5e9f78fc86d350f589fa1e5af81a45d
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-04 16:03:19 -0300

    feat: create route

--- app/controllers/clients_controllers/delete.py
+++ app/controllers/clients_controllers/delete.py
@@ -1,2 +1,15 @@
+from flask_jwt_extended import get_jwt_identity
+from sqlalchemy.orm.exc import NoResultFound
+
+from app.models import Client
+
+
 def delete():
     ...
+    try:
+        client_jwt = get_jwt_identity()
+
+        client = Client.query.filter_by(id=client_jwt['id']).first()
+
+    except NoResultFound:
+        return {"error": ""}
