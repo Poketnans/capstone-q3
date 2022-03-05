014351cc4b6c86ff673f3d884b97594773fa065f
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-05 12:14:05 -0300

    feat: finish get image client

---
+++ app/controllers/clients_controllers/get_image.py
@@ -0,0 +1,18 @@
+from http import HTTPStatus
+from flask import Response
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models import Client
+from sqlalchemy.orm.exc import NoResultFound
+
+
+@jwt_required()
+def get_image():
+    try:
+        client_jwt = get_jwt_identity()
+        id = client_jwt['id']
+        client: Client = Client.query.get(id)
+        if not client:
+            raise NoResultFound
+        return Response(client.image_bin, mimetype=client.image_mimetype), HTTPStatus.OK
+    except NoResultFound:
+        return {"msg": "image not found"}, HTTPStatus.NOT_FOUND
