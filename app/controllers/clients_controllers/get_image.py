f1a479f2545122f665d815cf22434311e21fce6d
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-05 15:21:59 -0300

    fix: search image client for name

--- app/controllers/clients_controllers/get_image.py
+++ app/controllers/clients_controllers/get_image.py
@@ -1,18 +1,15 @@
 from http import HTTPStatus
 from flask import Response
-from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from app.models import Client
 from sqlalchemy.orm.exc import NoResultFound
 
 
-@jwt_required()
-def get_image():
+def get_image(image_name: str):
     try:
-        client_jwt = get_jwt_identity()
-        id = client_jwt['id']
-        client: Client = Client.query.get(id)
+        client: Client = Client.query.filter_by(image_name=image_name).first()
         if not client:
             raise NoResultFound
         return Response(client.image_bin, mimetype=client.image_mimetype), HTTPStatus.OK
     except NoResultFound:
-        return {"msg": "client not found"}, HTTPStatus.NOT_FOUND
+        return {"msg": "image client not found"}, HTTPStatus.NOT_FOUND
