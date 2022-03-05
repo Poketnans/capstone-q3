85abfa13864604278866f8f850e7c7a019ecd4ed
Luiz <luizvictornacimento00@gmail.com>
2022-03-05 17:09:03 -0300

    fix: register blueprint

--- app/routes/tattoos_blueprint.py
+++ app/routes/tattoos_blueprint.py
@@ -1,3 +1,7 @@
 from flask import Blueprint
+from app.controllers.tattoos_controllers import update
+
 
 bp_tattoos = Blueprint('tattoos', __name__, url_prefix='/tattoos')
+
+bp_tattoos.patch("<id_tattoo>")(update)
