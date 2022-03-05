13a4283b6422879f5c52a3853f3431450d8632d4
Pedro <pedromenimen@gmail.com>
2022-03-05 15:47:05 -0300

    feat: get specific tattoo working as expected

--- app/controllers/tattoos_controllers/get_specific.py
+++ app/controllers/tattoos_controllers/get_specific.py
@@ -1,2 +1,35 @@
-def get_specific():
-    ...
+from http import HTTPStatus
+from flask import jsonify
+from flask_jwt_extended import jwt_required
+import werkzeug.exceptions
+from app.models.tattoos_model import Tattoo
+from app.models.sessions_model import Session
+from app.models.tattooists_model import Tattooist
+from sqlalchemy.exc import DataError
+
+
+@jwt_required()
+def get_specific(id_tattoo):
+    try:
+        tattoo = Tattoo.query.get_or_404(id_tattoo, description={"msg": "tattoo not found"})
+
+        return jsonify({
+            "id": tattoo.id,
+            "size": tattoo.size,
+            "colors": tattoo.colors,
+            "body_parts": tattoo.body_parts,
+            "tattoo_schedule": {
+                        "start": Session.query.get(tattoo.id_session).start,
+                        "end": Session.query.get(tattoo.id_session).end,
+                        "finished": Session.query.get(tattoo.id_session).finished,
+                        },
+            "tattoist": {
+                "name": Tattooist.query.get(tattoo.id_tattooist).name,
+                "email": Tattooist.query.get(tattoo.id_tattooist).email,
+                "image": Tattooist.query.get(tattoo.id_tattooist).url_image
+            }}
+        ), HTTPStatus.OK
+    except werkzeug.exceptions.NotFound as e:
+        return e.description, HTTPStatus.NOT_FOUND
+    except DataError:
+        return {"msg": "wrong id format"}, HTTPStatus.BAD_REQUEST
--- app/routes/tattoos_blueprint.py
+++ app/routes/tattoos_blueprint.py
@@ -1,3 +1,5 @@
 from flask import Blueprint
+from app.controllers.tattoos_controllers import get_specific
 
 bp_tattoos = Blueprint('tattoos', __name__, url_prefix='/tattoos')
+bp_tattoos.get("/<string:id_tattoo>")(get_specific) 
