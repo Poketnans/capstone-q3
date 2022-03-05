a428d7aff2c58efbf6393d9ca886a19caa698f20
BeatrixDev <beatrix.ford@hotmail.com>
2022-03-05 18:19:52 -0300

    fix: message adjustment according to standard

--- app/controllers/tattooists_controllers/get_specific.py
+++ app/controllers/tattooists_controllers/get_specific.py
@@ -12,7 +12,7 @@ def get_specific(id_tattooist:str):
     tattoist = Tattooist.query.get(id_tattooist)
 
     if not tattoist:
-        msg = {"error_message":  "Tattooist not found"}
+        msg = {"msg": "not found"}
         return jsonify(msg), HTTPStatus.NOT_FOUND
 
     return jsonify(tattoist), HTTPStatus.OK
