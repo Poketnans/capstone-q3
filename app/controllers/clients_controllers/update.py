c2818cdd141dea24aebacbcc8e4cf1d70c0f3cc2
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-05 10:59:55 -0300

    faet: finish updarte client

--- app/controllers/clients_controllers/update.py
+++ app/controllers/clients_controllers/update.py
@@ -1,2 +1,58 @@
+from http import HTTPStatus
+from flask import current_app, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from sqlalchemy.orm import Session
+
+from app.errors import JSONNotFound
+from app.models import Client
+from app.services.get_data_with_images import get_data_with_images, get_files
+from app.services.payload_eval import payload_eval
+
+
+@jwt_required()
 def update():
-    ...
+    try:
+        session: Session = current_app.db.session
+        client_jwt = get_jwt_identity()
+        id = client_jwt['id']
+
+        client: Client = Client.query.get(id)
+
+        data = get_data_with_images(exception=False)
+        if(data):
+            optional_fields = ["name", "email", "password", "phone", "general_information",
+                               "street", "number", "city", "image_name", "image_bin", "image_mimetype"]
+            field_types = {
+                'name': str,
+                'email': str,
+                'phone': str,
+                'password': str,
+                'street': str,
+                'number': int,
+                'city': str
+            }
+            data = payload_eval(data, optional_fields, **field_types)
+            for key, value in data.items():
+                setattr(client, key, value)
+
+        files = get_files()
+        if files:
+            for file in files:
+                client.image_bin = file.file_bin
+                client.image_name = file.filename
+                client.image_mimetype = file.mimetype
+        if not data and files:
+            raise JSONNotFound
+
+        # session.add(client)
+        # session.commit()
+        print("")
+        print("CLient -> ", data)
+        print("")
+        print("CLient -> ", client)
+        print("")
+
+        return jsonify(client), HTTPStatus.OK
+    except JSONNotFound as e:
+        return {"error": f"{e.describe}"}, e.status_code
--- app/services/get_data_with_images.py
+++ app/services/get_data_with_images.py
@@ -12,23 +12,26 @@ class ImageFile():
         self.mimetype = kargs['mimetype']
 
 
-def get_data_with_images() -> dict:
+def get_data_with_images(exception: bool = True, key_form: str = "data") -> "dict or None":
     ''' Função captura os dados de uma rota.
         A captura é feita caso a rota seja com `JSON` ou `Multipart-form` caso contrario lança um  exceção.
         As imagens são capturadas na função `get_files`.
+        `exception` campo boleano opcional que define se é levantado exceções. O valor por padrão é True.
 
         Exceções:
             `from app.errors.JSONNotFound` - Body vazio.
     '''
+
     data = None
     if(request.get_json()):
         data: dict = request.get_json()
-    elif request.form.get("tatooist"):
-        data: dict = loads(request.form.get("tatooist"))
+    elif request.form.get(key_form):
+        data: dict = loads(request.form.get(key_form))
         if data.get("file"):
             data.pop("file")
     else:
-        raise JSONNotFound
+        if exception:
+            raise JSONNotFound
     return data
 
 
