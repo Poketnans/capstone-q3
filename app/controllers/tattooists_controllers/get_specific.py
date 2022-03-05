5f7a307ca2aaf87616494d24c4cbd280332cc1fb
BeatrixDev <beatrix.ford@hotmail.com>
2022-03-05 11:52:58 -0300

    feat: insert code get specific tattoist

--- app/controllers/tattooists_controllers/get_specific.py
+++ app/controllers/tattooists_controllers/get_specific.py
@@ -1,2 +1,17 @@
-def get_specific():
-    ...
+from http import HTTPStatus
+
+from flask import jsonify
+from flask_jwt_extended import jwt_required
+
+from app.models.tattooists_model import Tattooist
+
+
+@jwt_required()
+def get_specific(id_tattooist:str):
+
+    tattoist = Tattooist.query.get(id_tattooist)
+
+    if not tattoist:
+        return jsonify([]), HTTPStatus.OK
+
+    return jsonify(tattoist), HTTPStatus.OK
