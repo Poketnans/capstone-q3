896c804e2a3ad2641f515e72c25e3beeec328215
Etnan Sousa <poketnans@gmail.com>
2022-03-06 13:57:14 -0300

    feat: implements tattoo create route

--- app/controllers/tattoos_controllers/__init__.py
+++ app/controllers/tattoos_controllers/__init__.py
@@ -1,5 +1,4 @@
 from .create import create
-from .delete import delete
 from .get_all import get_all
 from .get_specific import get_specific
 from .update import update
--- app/routes/tattoos_blueprint.py
+++ app/routes/tattoos_blueprint.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
 
-from app.controllers.tattoos_controllers import get_specific, get_all, update
+from app.controllers.tattoos_controllers import get_specific, get_all, update, create
 
 bp_tattoos = Blueprint('tattoos', __name__, url_prefix='/tattoos')
-bp_tattoos.get("/<id_tattoo>")(get_specific) 
+bp_tattoos.get("/<id_tattoo>")(get_specific)
 bp_tattoos.get("")(get_all)
 bp_tattoos.patch("<id_tattoo>")(update)
+bp_tattoos.post("")(create)
