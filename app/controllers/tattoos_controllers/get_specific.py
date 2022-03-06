8cc61b1c0ff8404d580be51ac5043bfc7eb44c9a
Pedro <pedromenimen@gmail.com>
2022-03-06 11:55:35 -0300

    refactor: added id client

--- app/controllers/tattoos_controllers/get_specific.py
+++ app/controllers/tattoos_controllers/get_specific.py
@@ -9,18 +9,19 @@ from sqlalchemy.exc import DataError
 @jwt_required()
 def get_specific(id_tattoo):
     try:
-        tattoo = Tattoo.query.get_or_404(id_tattoo, description={"msg": "tattoo not found"})
+        tattoo = Tattoo.query.get_or_404(
+            id_tattoo, description={"msg": "tattoo not found"})
 
         return jsonify({
             "id": tattoo.id,
             "size": tattoo.size,
             "colors": tattoo.colors,
             "body_parts": tattoo.body_parts,
+            "id_client": tattoo.id_client,
             "tattoo_schedule": tattoo.tattoo_schedule,
             "tattoist": tattoo.tattooist
-            }), HTTPStatus.OK
+        }), HTTPStatus.OK
     except werkzeug.exceptions.NotFound as e:
         return e.description, HTTPStatus.NOT_FOUND
     except DataError:
         return {"msg": "wrong id format"}, HTTPStatus.BAD_REQUEST
-    
