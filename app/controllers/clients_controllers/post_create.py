21baba1a8185826d94cb1c00a517e9d42589844e
Etnan Sousa <poketnans@gmail.com>
2022-03-06 00:09:30 -0300

    refactor: ajusts code for verify decorator using

--- app/controllers/clients_controllers/post_create.py
+++ app/controllers/clients_controllers/post_create.py
@@ -1,34 +1,39 @@
 from http import HTTPStatus
 
-from flask import jsonify, request
+from flask import jsonify
 from psycopg2.errors import UniqueViolation
 from sqlalchemy.exc import IntegrityError
 
 from app.classes.app_with_db import current_app
-from app.errors import FieldMissingError, InvalidValueTypesError
 from app.models.clients_model import Client
-from app.services.payload_eval import payload_eval
+from app.decorators import verify_payload
 
 
-def post_create():
+@verify_payload(
+    optional=[
+        "general_information",
+        "image_name",
+        "image_bin",
+        "image_mimetype",
+        "tattoos"],
+    **{
+        'name': str,
+        'email': str,
+        'birth_date': str,
+        'phone': str,
+        'password': str,
+        'street': str,
+        'number': int,
+        'city': str
+    })
+def post_create(payload):
     session = current_app.db.session
-    client_data = request.get_json()
 
-    try: 
-        field_types = {
-            'name': str,
-            'email': str,
-            'birth_date': str,
-            'phone': str,
-            'password': str,
-            'street': str,
-            'number': int,
-            'city': str
-        }
-        
-        optional_fields = ["general_information" ,"image_name","image_bin","image_mimetype", "tattoos"]
-        client_data = payload_eval(client_data, optional_fields , **field_types)
-        new_client = Client(**client_data)
+    from colorama import Fore
+    print(Fore.CYAN, payload)
+
+    try:
+        new_client = Client(**payload)
 
         session.add(new_client)
         session.commit()
@@ -37,11 +42,5 @@ def post_create():
         if isinstance(error.orig, UniqueViolation):
             msg = {"error_message": "key value already registered"}
             return jsonify(msg), HTTPStatus.CONFLICT
-    
-    except FieldMissingError as err:
-        return jsonify(err.description),err.code
 
-    except InvalidValueTypesError as err:
-        return jsonify(err.description),err.code
-    
     return jsonify(new_client), HTTPStatus.CREATED
