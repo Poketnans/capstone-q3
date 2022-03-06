5588ca35bf0d724bc1692b3a1130029b73e1ee76
Pedro <pedromenimen@gmail.com>
2022-03-06 11:53:23 -0300

    refactor: easier to get all tattoos

--- app/controllers/tattoos_controllers/get_all.py
+++ app/controllers/tattoos_controllers/get_all.py
@@ -2,8 +2,6 @@ from http import HTTPStatus
 from flask import jsonify
 from flask_jwt_extended import jwt_required
 from app.models.tattoos_model import Tattoo
-from app.models.sessions_model import Session
-from app.models.tattooists_model import Tattooist
 
 
 @jwt_required()
@@ -18,14 +16,6 @@ def get_all():
         "size": tattoo.size,
         "colors": tattoo.colors,
         "body_parts": tattoo.body_parts,
-        "tattoo_schedule": {
-                    "start": Session.query.get(tattoo.id_session).start,
-                    "end": Session.query.get(tattoo.id_session).end,
-                    "finished": Session.query.get(tattoo.id_session).finished,
-                    },
-        "tattoist": {
-                "name": Tattooist.query.get(tattoo.id_tattooist).name,
-                "email": Tattooist.query.get(tattoo.id_tattooist).email,
-                "image": Tattooist.query.get(tattoo.id_tattooist).url_image
-        }} for tattoo in tattoos
+        "tattoo_schedule": tattoo.tattoo_schedule,
+        "tattoist": tattoo.tattooist} for tattoo in tattoos
     ]), HTTPStatus.OK
