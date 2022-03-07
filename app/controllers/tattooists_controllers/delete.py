c6545c75c59ff635c3eb9d78cdc45418a352c05e
Ricardo Lima <ricardo.engp@gmail.com>
2022-03-07 09:43:24 -0300

    feature: endpoint tattooists delete done

--- app/controllers/tattooists_controllers/delete.py
+++ app/controllers/tattooists_controllers/delete.py
@@ -1,2 +1,20 @@
+from flask import current_app
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from sqlalchemy.orm import Session
+from http import HTTPStatus
+
+from app.models import Tattooist
+
+
+@jwt_required()
 def delete():
-    ...
+    session: Session = current_app.db.session
+    tatooist_jwt = get_jwt_identity()
+
+    tattooist: Tattooist = Tattooist.query.filter_by(id=tatooist_jwt['id']).first()
+    tattooist.password_hash = ""
+    session.add(tattooist)
+    session.commit()
+
+    return "", HTTPStatus.NO_CONTENT
