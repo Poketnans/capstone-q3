6624f0a52ca9a5a8b30f125c983388e9f3bda482
Etnan Sousa <poketnans@gmail.com>
2022-03-07 20:52:30 -0300

    fix: ajusts error management on clients controller

--- app/controllers/clients_controllers/get_specific.py
+++ app/controllers/clients_controllers/get_specific.py
@@ -5,12 +5,18 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
 from app.models import Client
+import werkzeug
 
 
 @jwt_required()
 def get_specific():
     id = get_jwt_identity().get("id")
 
-    client = Client.query.get(id)
+    try:
+        client = Client.query.filter_by(id=id).first_or_404(
+            description={"msg": "client not found"})
+
+    except werkzeug.exceptions.NotFound as err:
+        return err.description, HTTPStatus.NOT_FOUND
 
     return jsonify(client), HTTPStatus.OK
