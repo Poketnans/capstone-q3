6a1bd67efdae8939203de4067803d31ac9fc1934
Pedro <pedromenimen@gmail.com>
2022-03-07 10:41:36 -0300

    refactor: token refactor and verify_payload decorator

--- app/controllers/clients_controllers/post_login.py
+++ app/controllers/clients_controllers/post_login.py
@@ -1,38 +1,34 @@
 from http import HTTPStatus
 
 import werkzeug.exceptions
-#from ipdb import set_trace
 from flask import request
 from flask_jwt_extended import create_access_token
 
-from app.configs import Config
 from app.errors import FieldMissingError
 from app.models.clients_model import Client
-from app.services import payload_eval
+from app.decorators.verify_payload import verify_payload
 
 
-def post_login():
+@verify_payload(
+    fields_and_types = {
+        "email":str,
+        "password":str
+        },
+    optional=[])
+def post_login(payload):
     data = request.get_json()
-    # set_trace()
     try:
         if data == None:
             raise FieldMissingError(description={"msg": "the body was empty"})
 
-        clean_data = payload_eval.payload_eval(
-            data=data,
-            email=str,
-            password=str,
-            optional=[]
-        )
-
-        user = Client.query.filter_by(email=clean_data["email"]).first_or_404(
+        user = Client.query.filter_by(email=data["email"]).first_or_404(
             description={"msg": "user not found"})
 
-        if not user.verify_password(clean_data["password"]):
+        if not user.verify_password(data["password"]):
             return {"msg": "wrong password"}, HTTPStatus.FORBIDDEN
 
         token = create_access_token(
-            identity=user)
+            identity={"id": user.id})
 
         return {"access token": token}
     except werkzeug.exceptions.NotFound as e:
