84572009efbc0b9b66929084939597afd5b48024
Pedro <pedromenimen@gmail.com>
2022-03-08 19:03:36 -0300

    refactor: images now will be given by hashed img name+tattooist id

--- app/controllers/tattooists_controllers/get_image.py
+++ app/controllers/tattooists_controllers/get_image.py
@@ -4,11 +4,15 @@ from flask import Response
 from app.models import Tattooist
 from sqlalchemy.orm.exc import NoResultFound
 
-def get_image(image_name:str):
+
+def get_image(image_hash: str):
     try:
-        tattooists: Tattooist = Tattooist.query.filter_by(image_name=image_name).first()
-        if not tattooists:
+        tattooist_list: list(Tattooist) = [tattooist for tattooist in Tattooist.query.all(
+        ) if tattooist.image_hash == image_hash]
+        if tattooist_list:
+            tattooist = tattooist_list[0]
+        else:
             raise NoResultFound
-        return Response(tattooists.image_bin, mimetype=tattooists.image_mimetype), HTTPStatus.OK
+        return Response(tattooist.image_bin, mimetype=tattooist.image_mimetype), HTTPStatus.OK
     except NoResultFound:
         return {"msg": "image tattooist not found"}, HTTPStatus.NOT_FOUND
