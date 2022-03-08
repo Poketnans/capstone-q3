907fdbc96d9ad841bf06c9c231a46620a1134058
Pedro <pedromenimen@gmail.com>
2022-03-08 19:55:34 -0300

    rafactor: tattoo image now cab be searched with its hashed id + image name

--- app/controllers/tattoos_controllers/get_image.py
+++ app/controllers/tattoos_controllers/get_image.py
@@ -5,10 +5,13 @@ from app.models import TattooImage
 from sqlalchemy.orm.exc import NoResultFound
 
 
-def get_image(image_name: str):
+def get_image(image_hash: str):
     try:
-        tattoo_image: TattooImage = TattooImage.query.filter_by(image_name=image_name).first()
-        if not tattoo_image:
+        tattoo_list = [tattoo for tattoo in TattooImage.query.all(
+        ) if tattoo.image_hash == image_hash]
+        if tattoo_list:
+            tattoo_image = tattoo_list[0]
+        else:
             raise NoResultFound
         return Response(tattoo_image.image_bin, mimetype=tattoo_image.image_mimetype), HTTPStatus.OK
     except NoResultFound:
