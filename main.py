ce2bff6b028f9a06dd9d24e4124c6e721e2ec0a9
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-09 11:35:28 -0300

    feat:

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import base64
 import os
 from PIL import Image
 
-#image = Image.open("./app/assets/image_default.png")
-#print("Image: ", ima )
+image = Image.open("./app/assets/image_default.png")
+# print("Image: ", image.)
 
-with open("./app/assets/image_default.png", "r", encoding='utf-8') as file:
-    converted_string = base64 .b64encode(file.read())
-    print("FILE ", converted_string)
-    #new_file = file.read()
+# image.get_format_mimetype()
+
+with open("./app/assets/image_default.png", "rb") as file:
+    print("-> ", file.read())
+#    converted_string = base64 .b64encode(file.read())
+#    print("FILE ", converted_string)
+#new_file = file.read()
 # with open('encode.bin', "wb") as file:
 #    file.write(converted_string)
