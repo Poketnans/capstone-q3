25d330f39df1f1d52284120d4682e5332f7ff54d
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-09 17:58:15 -0300

    feat: create image default tatooist

--- app/controllers/tattooists_controllers/post_create.py
+++ app/controllers/tattooists_controllers/post_create.py
@@ -1,4 +1,3 @@
-
 from http import HTTPStatus
 
 from flask import current_app, jsonify
@@ -32,12 +31,12 @@ def post_create(payload):
         if files:
             for file in files:
                 new_tatooist.image_bin = file.file_bin
-                new_tatooist.image_name = file.filename
+                new_tatooist.image_hash = file.filename
                 new_tatooist.image_mimetype = file.mimetype
         else:
             image = generate_image_default()
             new_tatooist.image_mimetype = image.mimetype
-            new_tatooist.image_name = image.filename
+            new_tatooist.image_hash = image.filename
             new_tatooist.image_bin = image.file_bin
 
         session.add(new_tatooist)
