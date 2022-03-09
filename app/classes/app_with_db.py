c4c6ddd5cb422a6a89d43c2fe07d3fe38b0f4487
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-09 15:43:16 -0300

    fix: removed class imagefile

--- app/classes/app_with_db.py
+++ app/classes/app_with_db.py
@@ -15,12 +15,3 @@ class AppWithDb(Flask):
 
 ''' Intellisense do current app'''
 current_app: AppWithDb = current_app
-
-
-class ImageFile():
-    ''' Ultilizada para possibilitar o intelisense '''
-
-    def __init__(self, **kargs) -> None:
-        self.file_bin = kargs['file_bin']
-        self.filename = kargs['filename']
-        self.mimetype = kargs['mimetype']
