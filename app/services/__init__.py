d21a7d86e87920b2d43da58b8ba749089f378f87
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-09 14:03:42 -0300

    feat: create service generate default image

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,3 +2,4 @@ from .payload_eval import payload_eval
 from .get_orig_error_field import get_orig_error_field
 from .get_data import get_data
 from .get_files import get_files
+from .default_image import generate_image_default
