3e9d8f4297696d9640c61b3351f1974c39e87881
Brunoro811 <ferreira.bruno.oliveira@gmail.com>
2022-03-10 15:54:09 -0300

    feat: resize image

--- app/services/get_files.py
+++ app/services/get_files.py
@@ -1,4 +1,5 @@
-from json import loads
+from PIL import Image
+from environs import os
 
 from flask import request
 from werkzeug.utils import secure_filename
@@ -17,20 +18,31 @@ def get_files(limite=None) -> "list[ImageFile] or None":
         `limite` é opicional e referente a quantidade de imagens.
     '''
     list_files = []
+    path = "tmp"
     if request.files:
         count = 0
         for key, value in request.files.items():
             if(limite == count):
                 break
-            file = value
-            file_bin = file.read()
-            filename = secure_filename(file.filename)
-            mimetype = file.mimetype
+
+            with Image.open(value) as open_image:
+                (width, height) = (open_image.width//2, open_image.height//2)
+                image_resized = open_image.resize((width, height))
+                image_resized.save(os.path.join(
+                    path, secure_filename(value.filename)), quality=60)
+                path_file = f"{path}/{value.filename}"
+
+            with open(path_file, "rb") as file:
+                file_bin = file.read()
+                filename = secure_filename(value.filename)
+                mimetype = value.mimetype
+
             image = ImageFile(**{
                 "file_bin": file_bin,
                 "filename": filename,
                 "mimetype": mimetype
             })
+            os.remove(path_file)
             count += 1
             list_files.append(image)
         return list_files
