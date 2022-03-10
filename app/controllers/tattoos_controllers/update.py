743fc7989cebd4f5fed1f6719a05323bd7d009a1
Etnan Sousa <poketnans@gmail.com>
2022-03-10 12:52:38 -0300

    feat: implements storage validate and update

--- app/controllers/tattoos_controllers/update.py
+++ app/controllers/tattoos_controllers/update.py
@@ -11,7 +11,7 @@ import werkzeug
 
 from app.classes.app_with_db import current_app
 from app.decorators import verify_payload
-from app.errors import FieldMissingError, InvalidValueTypesError
+from app.errors import FieldMissingError, InvalidValueTypesError, UnavaliableItemQuantityError
 from app.models import Tattooist, Tattoo, Material, Storage
 from app.errors import NotAnAdmin
 from app.services import payload_eval
@@ -52,15 +52,21 @@ def update(id_tattoo, payload: dict):
                 material_info['id_tattoo'] = id_tattoo
 
                 # looking for existing storage item
-                Storage.query.filter_by(id=material_info['id_item']).first_or_404(
-                    description={"msg": "storage item not found"})
+                item: Storage = Storage.query.get_or_404(material_info['id_item'],
+                                                         description={"msg": "storage item not found"})
 
                 material_payload = payload_eval(
                     material_info,
                     **materials_fields
                 )
+
                 new_material = Material(**material_payload)
                 tattoo.materials.append(new_material)
+
+                if item.quantity < new_material.quantity:
+                    raise UnavaliableItemQuantityError(item)
+
+                item.quantity -= new_material.quantity
 
         for key, value in payload.items():
             setattr(tattoo, key, value)
@@ -73,6 +79,8 @@ def update(id_tattoo, payload: dict):
     except InvalidValueTypesError as err:
         return jsonify(err.description), err.code
     except FieldMissingError as err:
+        return jsonify(err.description), err.code
+    except UnavaliableItemQuantityError as err:
         return jsonify(err.description), err.code
     except NotAnAdmin:
         return jsonify({"msg": "you don't have the right privileges"}), HTTPStatus.FORBIDDEN
