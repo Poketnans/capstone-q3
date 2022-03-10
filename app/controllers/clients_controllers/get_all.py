e443ca364eb555dfd3077760484d10506286b0e8
Pedro <pedromenimen@gmail.com>
2022-03-10 15:07:18 -0300

    feat: maximum of 15 items per get all requisition

--- app/controllers/clients_controllers/get_all.py
+++ app/controllers/clients_controllers/get_all.py
@@ -1,17 +1,15 @@
 from http import HTTPStatus
 
-from flask import jsonify
+from flask import jsonify, request
 from flask_jwt_extended import jwt_required
 
 from app.models.clients_model import Client
 
-
 @jwt_required()
 def get_all():
-
-    clients = Client.query.all()
-
+    page = request.args.get('page', 1, type=int)
+    clients = Client.query.paginate(page=page, per_page=15)
     if not clients:
         return jsonify([]), HTTPStatus.OK
 
-    return jsonify(clients), HTTPStatus.OK
+    return jsonify(clients.items), HTTPStatus.OK
